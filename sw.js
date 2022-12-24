c70d5f8b7ec650c597b68291d84f15120ba6c509
Luca Martinengo <it.luca.martinengo@gmail.com>
2022-12-24 17:00:10 +0000

    Fix service worker

--- sw.js
+++ sw.js
@@ -1,18 +1,36 @@
 var cacheName = 'CVSite'
 var filesToCache = [
+  '/',
   '/index.html',
   '/js/main.js'
 ];
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-  self.skipWaiting();
-});
+// /* Start the service worker and cache all of the app's content */
+// self.addEventListener('install', function(e) {
+//   e.waitUntil(
+//     caches.open(cacheName).then(function(cache) {
+//       return cache.addAll(filesToCache);
+//     })
+//   );
+//   self.skipWaiting();
+// });
+
+self.addEventListener("install", (event) => {
+    console.log("Service Worker : Installed!")
+
+    event.waitUntil(
+        
+        (async() => {
+            try {
+                cache_obj = await caches.open(cache)
+                cache_obj.addAll(filesToCache)
+            }
+            catch{
+                console.log("error occured while caching...")
+            }
+        })()
+    )
+} )
 
 /* Serve cached content when offline */
 self.addEventListener('fetch', function(e) {
